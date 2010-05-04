fdb11f4153086b8185df756b41533127eb0fffc2
Ma Liang <bigml@eeif.org>
2010-05-04 23:37:53 +0800

    lcs - status & flag ok

--- modules/libape-lcs.h
+++ modules/libape-lcs.h
@@ -1,21 +1,27 @@
 #ifndef __LIBAPE_LCS_H__
 #define __LIBAPE_LCS_H__
 
-#define LCS_PIP_NAME "LiveCSPipe_"
-#define RAW_LCSDATA	"LCS_DATA"
+#define LCS_PIP_NAME	"LiveCSPipe_"
+#define RAW_LCSDATA		"LCS_DATA"
 
 enum {
-	LCS_SERVICE_LIMITED = 0,
-	LCS_SERVICE_NORMAL = 100,
+	LCS_ST_STRANGER = 0,
+	LCS_ST_LIMITED,
+	LCS_ST_FREE,				/* 30 online/month, 20 history raw, 2 admin */
+	LCS_ST_VIPED,				/* ED history raw */
+	LCS_ST_VIPING,				/* 300 online/month, 180 history raw, 20 admin */
+	LCS_ST_VVIPING				/* unlimit online/month, unlimit history raw, unlimit admin */
 } lcs_stat;
 
 enum {
-	ST_LCS_MSG_TOTAL = 100,
-	ST_LCS_ALIVE_GROUP
-} lcs_stastic;
+	LCS_SMS_CLOSE = 0,
+	LCS_SMS_OPEN,
+	LCS_ADM_OFFLINE,
+	LCS_ADM_ONLINE
+} lcs_flag;
 
 typedef struct {
 	unsigned long msg_total;
 } st_lcs;
 
-#endif
+#endif	/* __LIBAPE_LCS_H__ */
